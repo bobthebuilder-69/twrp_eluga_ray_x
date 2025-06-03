d3cc4bf1eecd412541f768bc855197f19c43a2e4
bobthebuilder-69 <s.jasvndr@gmail.com>
2025-06-04 02:11:01 +0530

    Delete twrp_eluga_ray_x_final/device/panasonic/elugarayx directory

--- twrp_eluga_ray_x_final/device/panasonic/elugarayx/Android.mk
+++
@@ -1,5 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-ifneq ($(filter elugarayx,$(TARGET_DEVICE)),)
-include $(call all-subdir-makefiles)
-endif
--- twrp_eluga_ray_x_final/device/panasonic/elugarayx/device.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_DEVICE := elugarayx
-PRODUCT_NAME := omni_elugarayx
-PRODUCT_BRAND := Panasonic
-PRODUCT_MODEL := Eluga Ray X
-PRODUCT_MANUFACTURER := Panasonic
-
-PRODUCT_RELEASE_NAME := ElugaRayX
